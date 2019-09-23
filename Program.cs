e29001e867c8e4b66b97895daf9e7808fd618670
Lukas Selin <lukas.selin@viskan.se>
2019-09-23 13:10:50 +0200

    add temprature and exit functions

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -13,9 +14,16 @@ namespace vaderstation
 
         }
 
-        static void listAllTemps()
+        static void listAllTemps(List<double> temprature)
         {
-            throw new Exception("Not implemented yet");
+            // throw new Exception("Not implemented yet");
+
+            Console.WriteLine("Previous temprature readings: ");
+            for (int i = 0; i < temprature.Count; i++)
+            {
+
+                Console.WriteLine("[{0}] {1}", i, temprature[i]);
+            }
 
         }
 
@@ -33,8 +41,16 @@ namespace vaderstation
 
         static void Main(string[] args)
         {
+            // var p = new Process();
+
+            // p.StartInfo = new ProcessStartInfo(@"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe", "dotnet run")
+            // {
+            //     UseShellExecute = false
+            // };
+
+            // p.Start();
             Console.CursorVisible = false;
-            string[] menuContent = {"Lägg till tempratur mätning", "Skriv ut alla tempraturer och medeltempraturer", "Ta bort tempraturmätning", "Avsluta"};
+            string[] menuContent = { "Lägg till tempratur mätning", "Skriv ut alla tempraturer och medeltempraturer", "Ta bort tempraturmätning", "Avsluta" };
             var menu = new Menu(menuContent);
 
             string menuHeader = "Väderstationen";
@@ -42,37 +58,45 @@ namespace vaderstation
 
             List<double> temp = new List<double>();
 
+            bool exit = false;
 
-
-            switch(menu.SelectedIndex)
+            do
             {
-                case 0:
-                // Lägg till temp
-                    string stringInput = Console.ReadLine();
-                    double input;
-                    try
-                    {
-                        input = Convert.ToDouble(stringInput);
-                        temp.Add(input);
-                    }catch{
-                        Console.WriteLine("You can only enter numbers");
-                    }
+                switch (menu.SelectedIndex)
+                {
+                    case 0:
+                        // Lägg till temp
+                        string stringInput = Console.ReadLine();
+                        double input;
+                        try
+                        {
+                            Console.WriteLine("Enter temprature reading: ");
+                            stringInput = Console.ReadLine();
+                            input = Convert.ToDouble(stringInput);
+                            temp.Add(input);
+                        }
+                        catch
+                        {
+                            Console.WriteLine("You can only enter numbers");
+                        }
 
-                    break;
-                case 1:
-                // Skriv ut alla temp och medeltemp
-                listAllTemps();
-                    break;
-                case 2:
-                // Tabort alla temp 
-                removeAllTemps();
-                    break;
-                case 3:
-                // Avsluta
-                    return;
-                default:
-                    break;
-            }
+                        break;
+                    case 1:
+                        // Skriv ut alla temp och medeltemp
+                        listAllTemps(temp);
+                        break;
+                    case 2:
+                        // Tabort alla temp 
+                        removeAllTemps();
+                        break;
+                    case 3:
+                        // Avsluta
+                            exit = true; 
+                        return;
+                    default:
+                        break;
+                }
+            }while(!exit);
             Console.ReadKey();                                      // Paus i programmet så att användaren hinner läsa
             Console.ForegroundColor = ConsoleColor.Gray;            // Byter tillbaka färg till gray
             Console.Clear();
@@ -80,6 +104,6 @@ namespace vaderstation
 
         }
 
-        
+
     }
 }
